704f39874406026114d501f3a9719fa3edfcc117
TK Yu <tky476@gmail.com>
2019-01-14 16:32:13 +0900

    Mnemonic words copy button

--- app/src/main/java/io/mystudy/tnn/myevmapplication/wallet/MnemonicActivity.java
+++ app/src/main/java/io/mystudy/tnn/myevmapplication/wallet/MnemonicActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,7 +26,7 @@ public class MnemonicActivity extends AppCompatActivity implements View.OnClickL
 
     TextView viewAddress;
     TextView viewMnemonic;
-    TextView btCopy;
+    Button btCopy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
